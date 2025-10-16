86a657a096313b65884bb311affd8951efc4c6c5
Myuze <Myuze@users.noreply.github.com>
2025-10-16 01:24:02 -0700

    Added Records to md breakpoint

--- IBL6/src/routes/powerrankings/+page.svelte
+++ IBL6/src/routes/powerrankings/+page.svelte
@@ -5,13 +5,12 @@
 
 	console.log(data);
 	const prContainerClass = 'flex flex-col overflow-x-auto w-full';
-	const prGridClass = '';
-	const prTeamContainerClass = 'flex flex-row';
-	const prAnchorClass = 'block';
-	const prTeamImageClass = 'col-start-2 w-[90%] object-contain mb-2';
-	const prRecordSectionClass =
-		'hidden grid grid-cols-4 md:grid-cols-4 gap-2 md:gap-4 lg:gap-6 xl:gap-8 2xl:gap-10';
-	const prRecordItemClass = 'flex flex-col items-center justify-center';
+	const prTeamContainerClass = 'flex flex-row flex-nowrap items-center justify-center';
+	const prAnchorClass = '';
+	const prTeamImageClass = 'col-start-2 w-[90%] object-contain mb-2 md:max-w-[50%]';
+	const prRecordSectionClass = 'hidden basis-xs lg:grid cols-start-3 grid-cols-4 md:grid-cols-4';
+	const prRecordHeaderClass = 'flex flex-row items-center justify-center font-bold';
+	const prRecordItemClass = 'flex flex-row items-center justify-center';
 </script>
 
 <div class={prContainerClass}>
@@ -22,12 +21,26 @@
 			class={prAnchorClass}
 		>
 			<div id="team-container" class={prTeamContainerClass}>
-				<h2 class="m-auto text-xl font-semibold">{index + 1}.</h2>
+				<h2 class="items-center pr-2 text-xl font-semibold">{index + 1}.</h2>
 				<img
 					src={`https://iblhoops.net/ibl5/images/logo/${ranking.TeamID}.jpg`}
 					alt={ranking.Team}
 					class={prTeamImageClass}
 				/>
+				<div id="record-container" class={prRecordSectionClass}>
+					<div id="record-item" class={prRecordItemClass}>
+						<span>{ranking.win}</span>-<span>{ranking.loss}</span>
+					</div>
+					<div id="record-item" class={prRecordItemClass}>
+						<span>{ranking.home_win}</span>-<span>{ranking.home_loss}</span>
+					</div>
+					<div id="record-item" class={prRecordItemClass}>
+						<span>{ranking.road_win}</span>-<span>{ranking.road_loss}</span>
+					</div>
+					<div id="record-item" class={prRecordItemClass}>
+						<span>{ranking.ranking}</span>
+					</div>
+				</div>
 			</div>
 		</a>
 	{/each}
